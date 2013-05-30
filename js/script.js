9294e1b4ad75be72c9cff475663fa8dc6fb9bff2
wenbin zhou <wenbin5243@gmail.com>
2013-05-30 16:19:03 +0800

    add

---
+++ js/script.js
@@ -0,0 +1,10 @@
+$(function(){
+	$('.m14').click(function(e) {
+        $('.m17').show();
+		return false;
+    });
+	$('.m17 .close').click(function(e) {
+        $('.m17').hide();
+		return false;
+    });
+	});
