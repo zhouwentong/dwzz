e60332a073e4f715f78e63e123e544fb950e233e
wenbin zhou <wenbin5243@gmail.com>
2013-05-31 17:56:57 +0800

    添加图集模块

--- js/script.js
+++ js/script.js
@@ -7,4 +7,10 @@ $(function(){
         $('.m17').hide();
 		return false;
     });
-	});
+	$('.g2_2 li').not('.li2').find('img').hover(function(){
+		var title=$(this).attr('title');
+		var src=$(this).attr('data-src');
+		$('.g2_2 .l2 img').attr('src',src);
+		$('.g2_2 .l2 h3').text(title);
+	})
+});
